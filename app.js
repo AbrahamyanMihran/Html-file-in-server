e66236108a5d7bb96398249a6ee9b4e444560bee
AbrahamyanMihran <abrahammihran@gmail.com>
2022-09-12 14:49:58 +0400

    +image

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-let http = require('http');
-let fs = require('fs');
+const fs = require("fs");
+const express = require("express");
+const app = express();
+let testFolder = `${__dirname}/images`;
 
-function onRequest(req ,res){
-    res.writeHead(200 , {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null , function(error , data){
-        if(error){
-            res.writeHead(404);
-            res.write('File not found');
-        } else {
-            res.write(data);
-        }
-        res.end();
+app.get((request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
     });
-}
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+});
 
-http.createServer(onRequest).listen(8000)
+app.use("/images", express.static("images")); 
 
-
-
+app.listen(8880)
